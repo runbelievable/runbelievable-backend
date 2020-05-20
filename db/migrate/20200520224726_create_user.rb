3dfcdb7fde5e7f917aea2616929166c43e5562fb
Alex Gallant <agallant1201@gmail.com>
2020-05-20 16:58:42 -0600

    Migrate user table to the DB
    Co-Authored-By: Pierce A (Him/He, BE) <palworth@users.noreply.github.com>

---
+++ db/migrate/20200520224726_create_user.rb
@@ -0,0 +1,14 @@
+class CreateUser < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :age
+      t.string :gender
+      t.integer :max_run_distance
+      t.string :estimated_mile_pace
+
+      t.timestamps
+    end
+  end
+end
