8dd42f564493f3696be218bf3a7ec2c90a9e9509
Alex Gallant <agallant1201@gmail.com>
2020-05-21 16:19:15 -0600

    Add factory bots for users and runs for testing efficiency

---
+++ spec/factories/users.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :user do
+    first_name { Faker::Creature::Dog.name }
+    last_name { Faker::Creature::Dog.name }
+    age { rand(1..100).to_s }
+    gender { ['Male', 'Female'].shuffle.pop }
+    max_run_distance { rand(1..50) }
+    estimated_mile_pace { ['10:00', '08:00', '09:00', '07:00', '06:00', '05:00'].shuffle.pop }
+    
+    # role { :default }
+  end
+end
