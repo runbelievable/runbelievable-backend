6ca4816eb866927de3f4c668b1140e37fec56d3f
Alex Gallant <agallant1201@gmail.com>
2020-05-21 16:23:44 -0600

    Change name of run factory

--- spec/factories/runs.rb
+++ spec/factories/runs.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :user do
+  factory :run do
     distance { rand(1.00..50.00).round(2) }
     location { Faker::Address.city }
     total_time { ['10:00', '20:00', '30:00', '40:00', '50:00', '15:00'].shuffle.pop }
