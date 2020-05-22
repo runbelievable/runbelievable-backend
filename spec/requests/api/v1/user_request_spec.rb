f22768d21b3e3cd662d723f1b0c4030949cf0308
Alex Gallant <agallant1201@gmail.com>
2020-05-22 11:54:12 -0600

    Add spec to test CRUD api endpoints for users

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -28,5 +28,75 @@ describe "Users API" do
     expect(users).to eq([])
   end
 
+  it "shows a specific user by their id" do
+    id = create(:user).id
+
+    get "/api/v1/users/#{id}"
+
+    expect(response).to be_successful
+    user = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(user[:id].to_i).to eq(id)
+  end
+
+  it "can create a new user" do
+    bob = create(:user)
+    user1 = create(:user)
+
+    user_params = { first_name: user1.first_name,
+                    last_name: user1.last_name,
+                    age: user1.age,
+                    gender: user1.gender,
+                    max_run_distance: user1.max_run_distance,
+                    estimated_mile_pace: user1.estimated_mile_pace}
+
+    post "/api/v1/users", params: user_params
+    user = User.last
+
+    expect(response).to be_successful
+    expect(user.first_name).to eq(user_params[:first_name])
+    expect(user.first_name).to_not eq(bob.first_name)
+  end
+
+  it "cannot create a new user without all of the attributes" do
+    user1 = create(:user)
+
+    user_params = { first_name: user1.first_name,
+                    last_name: user1.last_name,
+                    age: user1.age,
+                    gender: user1.gender,
+                    max_run_distance: user1.max_run_distance}
+
+    post "/api/v1/users", params: user_params
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+    json = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(json[:attributes].values.join).to eq("[:estimated_mile_pace, [\"can't be blank\"]]")
+  end
+
+  it "can update an existing item" do
+    id = create(:user).id
+    previous_name = User.last.first_name
+    user_params = { first_name: "Alex" }
+
+    put "/api/v1/users/#{id}", params: user_params
+    user = User.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(user.first_name).to_not eq(previous_name)
+    expect(user.first_name).to eq("Alex")
+  end
+
+  it "can destroy an user" do
+    user = create(:user)
+
+    expect(User.count).to eq(1)
+
+    delete "/api/v1/users/#{user.id}"
+
+    expect(response).to be_successful
+    expect(User.count).to eq(0)
+    expect{User.find(user.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
 
 end
