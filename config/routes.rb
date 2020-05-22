51dfe90976cb8e15c56610a244bc77a611101011
Pierce Alworth <pbalworth@gmail.com>
2020-05-22 13:15:39 -0500

    Add create resource to run

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index]
-      resources :runs, only: [:index]
+      resources :runs, only: [:index, :show, :create]
     end
   end
 end
