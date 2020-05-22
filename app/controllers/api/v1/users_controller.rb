c22db5d49e5e3944f623602653cda39f4f7b0e6e
Alex Gallant <agallant1201@gmail.com>
2020-05-22 11:55:26 -0600

    Add user controller for crud actions for user api endpoints

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,4 +2,32 @@ class Api::V1::UsersController < ApplicationController
   def index
     render json: UserSerializer.new(User.all)
   end
+
+  def show
+    render json: UserSerializer.new(User.find(params[:id]))
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: UserSerializer.new(User.create(user_params)), status: 201
+    else
+      user_message = FailedUser.new(user)
+      render json: FailedSerializer.new(user_message), status: 401
+    end
+  end
+
+  def update
+    render json: UserSerializer.new(User.update(params[:id], user_params))
+  end
+
+  def destroy
+    render json: UserSerializer.new(User.destroy(params[:id]))
+  end
+  
+  private
+
+  def user_params
+    params.permit(:first_name, :last_name, :age, :gender, :max_run_distance, :estimated_mile_pace)
+  end
 end
