82dbab82135b3fbf94e1320bc59bd40a19d18063
Pierce Alworth <pbalworth@gmail.com>
2020-05-22 15:41:36 -0500

    Route for destroy run

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :runs, only: [:index, :show, :create, :update]
+      resources :runs, only: [:index, :show, :create, :update, :destroy]
       resources :users, only: [:index, :show, :create, :update, :destroy]
     end
   end
