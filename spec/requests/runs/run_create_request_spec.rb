0cd96cd5ed8a095e8c46d64fd10c776ea9381fbd
Pierce Alworth <pbalworth@gmail.com>
2020-05-22 15:25:02 -0500

    Update sad path logic for create run method, fully functional

--- spec/requests/runs/run_create_request_spec.rb
+++ spec/requests/runs/run_create_request_spec.rb
@@ -24,19 +24,19 @@ describe "Runs API create functionality" do
     expect(path).to eq( "/api/v1/runs")
   end 
 
-  # it "posting invalid params to create a user returns error" do
-  #   create_list(:run, 3)
-  #   run = create(:run, average_pace: "08:00")
+  it "posting invalid params to create a user returns error" do
+    create_list(:run, 3)
+    run = create(:run, average_pace: "08:00")
 
-  #   post "/api/v1/runs/", params: {
-  #                       distance: run.distance,
-  #                       total_time: run.total_time,
-  #                       average_pace: run.average_pace
-  #   }
+    post "/api/v1/runs/", params: {
+                        distance: run.distance,
+                        total_time: run.total_time,
+                        average_pace: run.average_pace
+    }
 
-  #   expect(response).to_not be_successful
-  #   expect(response.status).to eq(401)
-  #   json = JSON.parse(response.body, symbolize_names: true)
-  #   expect(json[:data][:attributes].flatten.to_s).to eq("[:unauthorized, \"[:location, [\\\"can't be blank\\\"]]\"]")
-  # end
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(json[:data][:attributes].flatten.to_s).to eq("[:unauthorized, \"[:location, [\\\"can't be blank\\\"]]\"]")
+  end
 end
