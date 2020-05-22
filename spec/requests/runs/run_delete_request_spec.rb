6e17cc4cd7c24d080da7a43a03e86afd0b467fd2
Pierce Alworth <pbalworth@gmail.com>
2020-05-22 15:46:18 -0500

    Add spec for delete run functionality

---
+++ spec/requests/runs/run_delete_request_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Runs API destoy functionallity' do
+  before :each do
+    Run.destroy_all
+  end
+  it 'can destroy a run' do
+    run = create(:run)
+    expect(Run.count).to eq(1)
+    delete "/api/v1/runs/#{run.id}"
+    expect(response).to be_successful
+    expect(Run.count).to eq(0)
+    expect { Run.find(run.id) }.to raise_error(ActiveRecord::RecordNotFound)
+  end
+end
