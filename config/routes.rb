cacf881f56b3d7540f8eddb506bdddf8eccc8408
Alex Gallant <agallant1201@gmail.com>
2020-05-22 16:30:03 -0600

    Add route to show a user's runs
    Co-Authored-By: Pierce A (Him/He, BE) <palworth@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :runs, only: [:index, :show, :create, :update, :destroy]
       resources :users, only: [:index, :show, :create, :update, :destroy]
+      get '/users/:user_id/runs', to: 'user_runs#index'
+      # scope module: 'users' do
+      #   get "/runs", to: "user_runs#index"
+      # end
+      # namespace :users do
+      #   resources :runs, only: [:index]
+      # end
     end
   end
+
 end
