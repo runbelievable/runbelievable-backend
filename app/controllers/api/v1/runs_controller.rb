2bbc7b8fc71a006e19d8f3a1d0c3b2f306e9893c
Pierce Alworth <pbalworth@gmail.com>
2020-05-22 13:16:08 -0500

    Add create method to runs controller

--- app/controllers/api/v1/runs_controller.rb
+++ app/controllers/api/v1/runs_controller.rb
@@ -2,4 +2,24 @@ class Api::V1::RunsController < ApplicationController
   def index
     render json: RunSerializer.new(Run.all)
   end
+
+  def show
+    run = Run.find(params[:id])
+    render json: RunSerializer.new(run)
+  end
+
+  def create
+    run = Run.new(run_params)
+    if run.save
+      render json: RunSerializer.new(run), status: 201
+    else
+      run_message = FailedRun.new(run)
+      render json: FailedSerializer.new(run_message), status: 401
+    end
+  end
+
+  private
+  def run_params 
+      params.permit(:distance, :location, :total_time, :average_pace)
+  end
 end
