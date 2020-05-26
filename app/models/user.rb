1eb6fb91f8050a01b457a619bd68a7c04fb28221
Pierce Alworth <pbalworth@gmail.com>
2020-05-26 18:29:37 -0500

    Add password_digest and username to model
    Co-Authored-By: Alex Gallant <agallant121@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ApplicationRecord
-  validates_presence_of :first_name,
-                       :last_name,
-                       :age,
-                       :gender,
-                       :max_run_distance,
-                       :estimated_mile_pace
+  validates :username, uniqueness: true, presence: true
+  validates_presence_of :password, require: true
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :age
+  validates_presence_of :gender
+  validates_presence_of :max_run_distance
+  validates_presence_of :estimated_mile_pace
+
+  has_secure_password
 
   has_many :user_runs
   has_many :runs, through: :user_runs
+
 end
