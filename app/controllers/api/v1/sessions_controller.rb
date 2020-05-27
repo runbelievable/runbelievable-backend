fbf203ed84622044bd9b101a687bae054e6fac37
Pierce Alworth <pbalworth@gmail.com>
2020-05-27 12:35:29 -0500

    Add sessions controller with login and logout functionality
    Co-Authored-By: Alex Gallant <agallant121@users.noreply.github.com>

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,32 @@
+class Api::V1::SessionsController < ApplicationController
+    def new 
+      user = User.find_by(username: params[:username])
+      if user
+        redirect_to "/api/v1/users/#{user.id}"
+      end
+    end
+
+    def login
+      user = User.find_by(username: params[:username])
+      if !user.nil? && user.authenticate(params[:password])
+        login_sucsess(user)
+      else
+        login_failed
+      end
+    end
+    
+    def logout
+      session.delete(:current_user_id)
+      render json: {logout_message: "You have been logged out"}, status: 201
+    end
+
+private
+  def login_sucsess(user)
+    session[:current_user_id] = user.id 
+    render json: UserSerializer.new(user), status: 201
+  end
+
+  def login_failed
+    render json: { failed: 'Please check username and password. Login Failed' }, status: 401
+  end
+end
