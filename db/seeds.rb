bea90ba758d56dc5ec1281d027fa6d1b8ea1cdce
Alex Gallant <agallant1201@gmail.com>
2020-05-27 16:32:16 -0600

    Add users with runs to seed data
    Co-Authored-By: Pierce A (Him/He, BE) <palworth@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Run.destroy_all
+
 
 10.times do
   User.create(
@@ -31,3 +34,75 @@ end
       max_run_distance: rand(1..50),
       estimated_mile_pace: '10:00')
 end
+
+user1 = User.create(username: 'agallant',
+      first_name: 'Alex',
+      last_name: 'Baker',
+      password: 'password',
+      location: 'Denver',
+      age: '31',
+      gender: 'Male',
+      max_run_distance: 10,
+      estimated_mile_pace: '08:00')
+
+user2 = User.create(username: 'buffaloman',
+      first_name: 'Bob',
+      last_name: 'Stinson',
+      password: 'password',
+      location: 'Denver',
+      age: '30',
+      gender: 'Male',
+      max_run_distance: 5,
+      estimated_mile_pace: '10:00')
+
+user3 = User.create(username: 'baluga',
+      first_name: 'Michelle',
+      last_name: 'Seinfeld',
+      password: 'password',
+      location: 'Denver',
+      age: '32',
+      gender: 'Female',
+      max_run_distance: 10,
+      estimated_mile_pace: '09:00')
+
+      #user1 runs
+user1.runs.create(distance: 10,
+    location: 'Denver',
+    total_time: '50:00',
+    average_pace: '10:00')
+user1.runs.create(distance: 5,
+    location: 'Denver',
+    total_time: '40:00',
+    average_pace: '10:00')
+user1.runs.create(distance: 3,
+    location: 'Denver',
+    total_time: '20:00',
+    average_pace: '10:00')
+
+    #user2 runs
+user2.runs.create(distance: 10,
+    location: 'Denver',
+    total_time: '50:00',
+    average_pace: '10:00')
+user2.runs.create(distance: 5,
+    location: 'Denver',
+    total_time: '40:00',
+    average_pace: '10:00')
+user2.runs.create(distance: 3,
+    location: 'Denver',
+    total_time: '20:00',
+    average_pace: '10:00')
+
+    #user3 runs
+user3.runs.create(distance: 10,
+    location: 'Denver',
+    total_time: '50:00',
+    average_pace: '10:00')
+user3.runs.create(distance: 5,
+    location: 'Denver',
+    total_time: '40:00',
+    average_pace: '10:00')
+user3.runs.create(distance: 3,
+    location: 'Denver',
+    total_time: '20:00',
+    average_pace: '10:00')
