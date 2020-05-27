906fad4d7d8fc2832631e4187af3b685fb8d6bd3
Alex Gallant <agallant1201@gmail.com>
2020-05-27 16:11:27 -0600

    Add user seed data with some random and some specific users to test attributes
    Co-Authored-By: Pierce A (Him/He, BE) <palworth@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  User.create(
+      username: Faker::Creature::Dog.name,
+      first_name: Faker::Creature::Cat.name,
+      last_name: Faker::Creature::Dog.name,
+      password: Faker::Creature::Dog.name,
+      location: Faker::Address.city,
+      age: rand(1..100).to_s,
+      gender: ['Male', 'Female'].shuffle.pop,
+      max_run_distance: rand(1..50),
+      estimated_mile_pace: ['10:00', '08:00', '09:00', '07:00', '06:00', '05:00'].shuffle.pop)
+end
+
+5.times do
+  User.create(
+      username: Faker::Creature::Dog.name,
+      first_name: Faker::Creature::Cat.name,
+      last_name: Faker::Creature::Dog.name,
+      password: Faker::Creature::Dog.name,
+      location: 'Denver',
+      age: rand(1..100).to_s,
+      gender: ['Male', 'Female'].shuffle.pop,
+      max_run_distance: rand(1..50),
+      estimated_mile_pace: '10:00')
+end
