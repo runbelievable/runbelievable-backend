b7b5fcdac4d42c41b8220c66f9df01b31cdf5805
Pierce Alworth <pbalworth@gmail.com>
2020-05-27 12:37:46 -0500

    User register and login spec
    Co-Authored-By: Alex Gallant <agallant121@users.noreply.github.com>

--- spec/requests/api/v1/user_register_and_sign_in_request_spec.rb
+++ spec/requests/api/v1/user_register_and_sign_in_request_spec.rb
@@ -18,21 +18,60 @@ RSpec.describe 'User registration endpoint' do
     expect(user.username).to eq('fun')
   end
 
-#   it 'Bad registration attempts not sucessfull and render faileduser serializer' do
+  it 'Bad registration attempts not sucessfull and render faileduser serializer' do
 
-#     post '/api/v1/users', params: {
-#                                     email: 'whatever@example.com',
-#                                     password: 'password',
-#                                     password_confirmation: 'notthesame'}.to_json,
-#                          headers: {
-#                                   'Content-Type' => 'application/json',
-#                                   'Accept' => 'application/json'
-#                                   }
+    post '/api/v1/users', params: { first_name: "john",
+                                    last_name: "brown",
+                                    age: "99",
+                                    username: "fun",
+                                    password: "fun2",
+                                    gender: "man",
+                                    estimated_mile_pace: "08:00"}
 
-#     expect(response).to_not be_successful
-#     expect(response.status).to eq(401)
-#     json = JSON.parse(response.body, symbolize_names: true)
-#     expect(json[:data][:attributes].values.join).to eq("doesn't match Password")
-#   end
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(json[:data][:attributes].values.join).to eq("[:max_run_distance, [\"can't be blank\"]]")
+  end
 
+  it 'existing user can login' do
+
+    user = create(:user, username: "timmy", password: "tim")
+    
+    post '/api/v1/login', params: {username: user.username, password: user.password}
+
+    expect(response).to be_successful
+    user_resp = User.last
+    expect(session[:current_user_id]).to eq(user_resp.id)
+    expect(user_resp.username).to eq('timmy')
+  end
+
+  it 'existing user cant login with bad credentials' do
+
+    user = create(:user, username: "timmy", password: "tim")
+    
+    post '/api/v1/login', params: {username: user.username, password: "hi"}
+
+    expect(response).to_not be_successful
+    user_resp = User.last
+    expect(session[:current_user_id]).to eq(nil)
+  end
+
+  it 'existing user with session can log out ' do
+
+    user = create(:user, username: "timmy", password: "tim")
+    
+    post '/api/v1/login', params: {username: user.username, password: user.password}
+
+    expect(response).to be_successful
+    user_resp = User.last
+    expect(session[:current_user_id]).to eq(user_resp.id)
+    expect(user_resp.username).to eq('timmy')
+
+    get "/api/v1/logout"
+
+    expect(response).to be_successful
+    expect(response.body).to eq("{\"logout_message\":\"You have been logged out\"}")
+    expect(session[:current_user_id]).to eq(nil)
+  end
 end
