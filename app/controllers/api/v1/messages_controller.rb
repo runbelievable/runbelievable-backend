782714636ac0dd914714849fab73b6309b8fbdfe
Pierce Alworth <pbalworth@gmail.com>
2020-05-30 14:38:42 -0500

    Add messages controller
    Co-Authored-By: Alex Gallant <agallant121@users.noreply.github.com>

---
+++ app/controllers/api/v1/messages_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::MessagesController < ApplicationController 
+    def index 
+        render json: MessagesSerializer.new(current_user.messages)
+    end
+
+    def inbox #all recived messages 
+        render json: MessagesSerializer.new(current_user.received_messages)
+    end
+
+    def outbox #all sent messages 
+        render json: MessagesSerializer.new(current_user.sent_messages) 
+    end
+
+    def reply_to_message #rely to a specific person 
+        reply_msg = current_user.messages.find_by(id: params[:message_id]).reply({topic: params[:topic], body: params[:body]})
+        render json: MessagesSerializer.new(reply_msg.conversation) 
+    end 
+
+    def show_msg_conversation 
+        render json: MessagesSerializer.new(current_user.messages.find_by(id: params[:message_id]).conversation) 
+    end
+
+    def start_message_conv #Post to start message convo 
+        recipient = User.find_by(username: params[:username])
+        new_message = current_user.send_message(recipient, {topic: params[:topic], body: params[:body]})
+        render json: MessagesSerializer.new(new_message) 
+    end
+
+end 
