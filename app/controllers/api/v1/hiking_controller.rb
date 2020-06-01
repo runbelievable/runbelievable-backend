87d744ccce3931f82d144f93ec98d00ef20d72d2
Pierce Alworth <pbalworth@gmail.com>
2020-06-01 18:30:16 -0500

    Remove degbuggin tool
    Co-Authored-By: Alex Gallant <agallant121@users.noreply.github.com>

--- app/controllers/api/v1/hiking_controller.rb
+++ app/controllers/api/v1/hiking_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::HikingController < ApplicationController
     def show
-        # binding.pry
         service = HikeMicroService.new(current_user.location)
         hike = Hike.new(service.get_single_hiking_trail)
         render json: HikeSerializer.new(hike)        
