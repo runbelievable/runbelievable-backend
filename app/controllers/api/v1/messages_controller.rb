c716f60ba153d7120a7cb733cf9adda62e8c52e5
Pierce Alworth <pbalworth@gmail.com>
2020-06-03 17:07:29 -0500

    Add new controller action

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -21,6 +21,15 @@ class Api::V1::MessagesController < ApplicationController
       render json: MessagesSerializer.new(reply_msg.conversation)
   end
 
+  def conversation_between #rely to a specific person
+     user = User.find(params[:user_id])
+     recipient = params[:username]
+     from = user.messages.are_from(User.find_by(username: recipient))
+     to = user.messages.are_to(User.find_by(username: recipient))
+     conversation_msgs = to + from
+      render json: MessagesSerializer.new(conversation_msgs)
+  end
+
   def show_msg_conversation
       conversation_msgs = current_user.messages.find_by(id: params[:message_id]).conversation
       render json: MessagesSerializer.new(conversation_msgs)
